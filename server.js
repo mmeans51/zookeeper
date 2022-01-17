8a353b6943a43b23f7c37278bc2c8822b7dcb67f
michael Means <colemeans51@gmail.com>
2022-01-17 15:28:31 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,35 @@
-const express = require('express');
+const { animals } = require("./data/animals.json");
+const express = require("express");
 const app = express();
-const { animals } = require('./data/animals');
 
-app.get('/api/animals', (req, res) => {
-    res.send('Hello!');
-})
+app.get("/api/animals", (req, res) => {
+  let results = animals;
+  if (req.query) {
+    results = filterByQuery(requ.query, results);
+  }
+  res.json("results");
+});
 
 app.listen(3001, () => {
-    console.log(`API server now on port 3001`);
+  console.log(`API server now on port 3001!`);
 });
+
+function filterByQuery(query, animalsArray) {
+  let filteredResults = animalsArray;
+  if (query.diet) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
+  }
+  if (query.species) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
+  }
+  if (query.name) {
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
+  }
+  return filteredResults;
+}
