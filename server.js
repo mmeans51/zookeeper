c6df51cec5be604b74368dfea0cbf181441e5179
michael Means <colemeans51@gmail.com>
2022-01-18 15:45:18 -0600

    save

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const { animals } = require('./data/animals');
+const { animals } = require("./data/animals");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const fs = require('fs');
-const path =  require('path');
+const fs = require("fs");
+const path = require("path");
 
 //parse incoming string or array data
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 //parse incoming json data
 app.use(express.json());
 
-app.get('/api/animals', (req, res) => {
+app.get("/api/animals", (req, res) => {
   let results = animals;
   if (req.query) {
     results = filterByQuery(req.query, results);
@@ -18,9 +18,8 @@ app.get('/api/animals', (req, res) => {
   res.json(results);
 });
 
-
 function findById(id, animalsArray) {
-  const result = animalsArray.filter(animal => animal.id === id)[0];
+  const result = animalsArray.filter((animal) => animal.id === id)[0];
   return result;
 }
 
@@ -28,13 +27,13 @@ function createNewAnimal(body, animalsArray) {
   const animal = body;
   animalsArray.push(animal);
   fs.writeFileSync(
-    path.join(__dirname, './data/animals.json'),
+    path.join(__dirname, "./data/animals.json"),
     json.stringify({ animals: animalsArray }, null, 2)
   );
 
   return animal;
 }
-app.get('/api/animals/:id', (req, res) => {
+app.get("/api/animals/:id", (req, res) => {
   const result = findById(req.params.id, animals);
   if (result) {
     res.json(result);
@@ -43,13 +42,16 @@ app.get('/api/animals/:id', (req, res) => {
   }
 });
 
-app.post('/api/animals', (req, res) => {
+app.post("/api/animals", (req, res) => {
   //req.body is where our incoming content will be
   req.body.id = animals.length.toString();
 
-  const animal = createNewAnimal(req.body, animals);
-
-  res.json(req.body);
+  if (!validateAnimal(req.body)) {
+    res.status(400).send('the animal is not properly formatted');
+  } else {
+    const animal = createNewAnimal(req.body, animals);
+    res.json(animal);
+  }
 });
 
 app.listen(PORT, () => {
@@ -63,27 +65,49 @@ function filterByQuery(query, animalsArray) {
     // save personality traits as a dedicated array
 
     //if personality traits is a string, place it into a new array and save
-    if (typeof query.personalityTraits === 'string') {
+    if (typeof query.personalityTraits === "string") {
       personalityTraitsArray = [query.personalityTraits];
     } else {
       personalityTraitsArray = query.personalityTraits;
     }
     //loop through each trait in the personalityTraits array:
-    personalityTraitsArray.forEach(trait => {
+    personalityTraitsArray.forEach((trait) => {
       filteredResults = filteredResults.filter(
-        animal => animal.personalityTraits.indexOf(trait) !== -1
+        (animal) => animal.personalityTraits.indexOf(trait) !== -1
       );
     });
   }
 
   if (query.diet) {
-    filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.diet === query.diet
+    );
   }
   if (query.species) {
-    filteredResults = filteredResults.filter(animal => animal.species === query.species);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.species === query.species
+    );
   }
   if (query.name) {
-    filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    filteredResults = filteredResults.filter(
+      (animal) => animal.name === query.name
+    );
   }
   return filteredResults;
+}
+
+function validateAnimal(animal) {
+  if (!animal.name || typeof animal.name !== "string") {
+    return false;
+  }
+  if (!animal.species || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.diet || typeof animal.species !== "string") {
+    return false;
+  }
+  if (!animal.personalityTraits || !Array.isArray(animal.personalityTraits)) {
+    return false;
+  }
+  return false;
 }
